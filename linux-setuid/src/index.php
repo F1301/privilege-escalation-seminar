d7d045950b51607926360bc51f10518cb722a33e
timoxoszt <20520810@gm.uit.edu.vn>
2023-10-09 22:59:35 +0700

    linux setuid

---
+++ linux-setuid/src/index.php
@@ -0,0 +1,234 @@
+<?php
+// Create folder for each user
+session_start();
+if (!isset($_SESSION['dir'])) {
+    $_SESSION['dir'] = 'upload/' . session_id();
+}
+$dir = $_SESSION['dir'];
+if (!file_exists($dir))
+    mkdir($dir);
+
+if (isset($_GET["debug"])) die(highlight_file(__FILE__));
+if (isset($_FILES["file"])) {
+    $error = '';
+    $success = '';
+    try {
+        $file = $dir . "/" . $_FILES["file"]["name"];
+        move_uploaded_file($_FILES["file"]["tmp_name"], $file);
+        $success = 'Successfully uploaded file at: <a href="/' . $file . '">/' . $file . ' </a><br>';
+        $success .= 'View all uploaded file at: <a href="/' . $dir . '/">/' . $dir . ' </a>';
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>F1301 File Storage</title>
+
+    <!-- This is for UI only -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        body {
+        font-family: sans-serif;
+        background-color: #eeeeee;
+        }
+
+        .file-upload {
+        background-color: #ffffff;
+        width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        }
+
+        .file-upload-btn {
+        width: 100%;
+        margin: 0;
+        color: #fff;
+        background: #1FB264;
+        border: none;
+        padding: 10px;
+        border-radius: 4px;
+        border-bottom: 4px solid #15824B;
+        transition: all .2s ease;
+        outline: none;
+        text-transform: uppercase;
+        font-weight: 700;
+        }
+
+        .file-upload-btn:hover {
+        background: #1AA059;
+        color: #ffffff;
+        transition: all .2s ease;
+        cursor: pointer;
+        }
+
+        .file-upload-btn:active {
+        border: 0;
+        transition: all .2s ease;
+        }
+
+        .file-upload-content {
+        display: none;
+        text-align: center;
+        }
+
+        .file-upload-input {
+        position: absolute;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        outline: none;
+        opacity: 0;
+        cursor: pointer;
+        }
+
+        .image-upload-wrap {
+        margin-top: 20px;
+        border: 4px dashed #1FB264;
+        position: relative;
+        }
+
+        .image-dropping,
+        .image-upload-wrap:hover {
+        background-color: #1FB264;
+        border: 4px dashed #ffffff;
+        }
+
+        .image-title-wrap {
+        padding: 0 15px 15px 15px;
+        color: #222;
+        }
+
+        .drag-text {
+        text-align: center;
+        }
+
+        .drag-text h3 {
+        font-weight: 100;
+        text-transform: uppercase;
+        color: #15824B;
+        padding: 60px 0;
+        }
+
+        .file-upload-image {
+        max-height: 200px;
+        max-width: 200px;
+        margin: auto;
+        padding: 20px;
+        }
+
+        .remove-image {
+        width: 200px;
+        margin: 0;
+        color: #fff;
+        background: #cd4535;
+        border: none;
+        padding: 10px;
+        border-radius: 4px;
+        border-bottom: 4px solid #b02818;
+        transition: all .2s ease;
+        outline: none;
+        text-transform: uppercase;
+        font-weight: 700;
+        }
+
+        .remove-image:hover {
+        background: #c13b2a;
+        color: #ffffff;
+        transition: all .2s ease;
+        cursor: pointer;
+        }
+
+        .remove-image:active {
+        border: 0;
+        transition: all .2s ease;
+        }
+    </style>
+
+    <script>
+        function readURL(input) {
+        if (input.files && input.files[0]) {
+
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            $('.image-title').html(input.files[0].name);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+
+        } else {
+            removeUpload();
+        }
+        }
+
+        function removeUpload() {
+        $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+        $('.file-upload-content').hide();
+        $('.image-upload-wrap').show();
+        }
+        $('.image-upload-wrap').bind('dragover', function () {
+            $('.image-upload-wrap').addClass('image-dropping');
+        });
+        $('.image-upload-wrap').bind('dragleave', function () {
+            $('.image-upload-wrap').removeClass('image-dropping');
+        });
+    </script>
+</head>
+
+<body>
+    <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="f1301-cloud.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        F1301 File Storage
+    </a>
+    </nav>
+    <br />
+    <br />
+    <br />
+    <script class="jsbin" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <div class="file-upload">
+    <button class="file-upload-btn" type="button" onclick="$('.file-upload-input').trigger( 'click' )">Add Image</button>
+    <div class="container">
+        <a href="/?debug">Debug source</a>
+        <div class="image-upload-wrap">
+        <form method="post" enctype="multipart/form-data">
+            <input class="file-upload-input" type='file' name='file' id='file' onchange="readURL(this);" accept="image/*" />
+            <div class="drag-text">
+            <h3>Drag and drop a file or select add Image</h3>
+            </div>
+        </div>
+        <div class="file-upload-content">
+            <img class="file-upload-image" src="#" alt="your image" />
+            <div class="image-title-wrap">
+            <button type="button" onclick="removeUpload()" class="remove-image">Remove <span class="image-title">Uploaded Image</span></button>
+            </div>
+        </div>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <br>
+        <span style="color:red"><?php echo $error; ?></span>
+        <span style="color:green"><?php echo $success; ?></span>
+    </div>
+
+</body>
+
+</html>
